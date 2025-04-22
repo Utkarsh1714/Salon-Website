3a8b3bd31ae8a424df09e83d02aba8d388e43de8
Utkarsh1714 <utkarshpalav17@gmail.com>
2025-04-23 00:57:38 +0530

    form UI created

--- frontend/app/appointment-booking/page.jsx
+++ frontend/app/appointment-booking/page.jsx
@@ -1,5 +1,7 @@
 "use client";
 
+import AnimatedContent from "@/reactbits/AnimatedContent/AnimatedContent";
+import BlurText from "@/reactbits/BlurText/BlurText";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
@@ -42,33 +44,76 @@ const page = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit} className="p-4 max-w-md mx-auto space-y-4">
-      <h1>Book Appointment</h1>
-      <input placeholder="Your Name" type="text" value={name} onChange={(e) => setName(e.target.value)} className="w-full p-2 border" required />
-      <input
-        type="date"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
-        className="w-full p-2 border"
-        required
-      />
-      <textarea
-        placeholder="Optional Note"
-        value={note}
-        onChange={(e) => setNote(e.target.value)}
-        className="w-full p-2 border"
-      ></textarea>
-      <button
-        type="submit"
-        className="bg-blue-600 text-white px-4 py-2 rounded"
-      >
-        {loading ? (
-          <div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
-        ) : (
-          "Book Appointment"
-        )}
-      </button>
-    </form>
+    <div className="w-full h-screen flex items-center justify-center px-10">
+      <div className="flex items-center justify-center gap-20">
+        <div className="p-4 md:w-1/2 max-w-md mx-auto space-y-4">
+          {/* <h1 className="text-xl md:text-4xl text-center text-[#5B3728] font-medium">Book Appointment</h1> */}
+          <BlurText
+            text="Book Your Appointment"
+            delay={150}
+            animateBy="words"
+            direction="top"
+            className="text-4xl md:text-4xl text-center text-[#5B3728] font-medium"
+          />
+          <AnimatedContent
+            distance={150}
+            direction="vertical"
+            reverse={false}
+            config={{ tension: 80, friction: 20 }}
+            initialOpacity={0.2}
+            animateOpacity
+            scale={1.1}
+            threshold={0.2}
+          >
+            <p className="text-[#5B3728]">
+              Schedule a time that works best for you. Once booked, you&apos;ll
+              receive a confirmation email, and you can view your appointment
+              details anytime in your account portal.
+            </p>
+            <form onSubmit={handleSubmit} className="space-y-4 pt-4">
+              <input
+                placeholder="Your Name"
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="w-full p-2 border"
+                required
+              />
+              <input
+                type="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                className="w-full p-2 border"
+                required
+              />
+              <textarea
+                placeholder="Optional Note"
+                value={note}
+                onChange={(e) => setNote(e.target.value)}
+                className="w-full p-2 border"
+              ></textarea>
+              <button
+                type="submit"
+                className="bg-blue-600 text-white px-4 py-2 rounded"
+              >
+                {loading ? (
+                  <div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
+                ) : (
+                  "Book Appointment"
+                )}
+              </button>
+            </form>
+          </AnimatedContent>
+        </div>
+        <div className="w-1/2 rounded-tl-2xl rounded-br-2xl hidden md:block items-center justify-center mx-auto">
+          <img
+            src="/appointment1.jpeg"
+            alt="addmission"
+            className="w-full lg:w-[400px] rounded-tl-[70px] rounded-br-[70px]"
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
--- frontend/ui/authButton/auth-btn.jsx
+++ frontend/ui/authButton/auth-btn.jsx
@@ -1,36 +1,3 @@
-// "use client";
-
-// import { Button } from "@/components/ui/button";
-// import { UserCircleIcon } from "lucide-react";
-// import { UserButton, SignInButton, SignedIn, SignedOut } from "@clerk/nextjs";
-
-// export const AuthButton = () => {
-//   //TODO: Add different auth states
-//   return (
-//     <>
-//       <SignedIn>
-//         <div className="flex items-center justify-center gap-2">
-//           <UserButton />
-//           <h4 className="text-[#5B3728] cursor-pointer">Profile</h4>
-//         </div>
-//         {/* Add menu items for studio and user profiles */}
-//       </SignedIn>
-//       <SignedOut>
-//         <SignInButton mode="modal">
-//           <Button
-//             variant={"outline"}
-//             className="px-4 py-2 text-sm font-medium text-[#5B3728] cursor-pointer hover:text-blue-500 border-[#E5C682] rounded-full shadow-none"
-//           >
-//             <UserCircleIcon />
-//             Sign In
-//           </Button>
-//         </SignInButton>
-//       </SignedOut>
-//     </>
-//   );
-// };
-
-
 "use client";
 
 import { Button } from "@/components/ui/button";
